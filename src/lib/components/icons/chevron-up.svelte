f73f80cc8e3e70f8e0e3fe117023f1d7c03f873d
Verre2OuiSki <verre2ouiski@gmail.com>
2023-05-01 15:19:55 +0200

    Update & Add icons

--- src/lib/components/icons/chevron-up.svelte
+++ src/lib/components/icons/chevron-up.svelte
@@ -16,9 +16,7 @@
   stroke-linejoin="round"
 >
   <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-  <path d="M12 6h-6a2 2 0 0 0 -2 2v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-6" />
-  <path d="M11 13l9 -9" />
-  <path d="M15 4h5v5" />
+  <path d="M6 15l6 -6l6 6" />
 </svg>
 
 
--- src/lib/components/icons/index.ts
+++ src/lib/components/icons/index.ts
@@ -1,1 +1,2 @@
 export { default as ExternalLink } from "./external-link.svelte";
+export { default as ChevronUp } from "./chevron-up.svelte";
