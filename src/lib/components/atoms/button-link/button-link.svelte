ebfcf51252e71502eb10bc5b37172b4ab73ae168
Verre2OuiSki <verre2ouiski@gmail.com>
2023-05-01 15:18:51 +0200

    Create ButtonLink component

---
+++ src/lib/components/atoms/button-link/button-link.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import clsx from "clsx";
+
+  export let link: string;
+  let clazz: string | null = null;
+  export {clazz as class};
+
+  const style = clsx(
+    "inline-block border-white hover:bg-white/10 border-2 rounded p-2",
+    clazz
+  );
+
+</script>
+
+<a class={style} href={link}>
+  <slot/>
+</a>
+
