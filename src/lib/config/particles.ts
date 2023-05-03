c5e3c91bff0066c3f52823f980f949e3ce9a9374
Verre2OuiSki <48868686+Unarray@users.noreply.github.com>
2023-05-03 10:30:38 +0200

    Remove particle config from static I moved config to have typecheking on the particle config

---
+++ src/lib/config/particles.ts
@@ -0,0 +1,53 @@
+import type { ISourceOptions } from "tsparticles-engine";
+
+/* eslint camelcase: "off" */
+export const config: ISourceOptions = {
+  fullScreen: {
+    enable: false
+  },
+  particles: {
+    number: {
+      value: 50,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    },
+    opacity: {
+      value: 0.2,
+      random: false,
+      anim: {
+        enable: false,
+        speed: 0.1,
+        opacity_min: 0.1,
+        sync: false
+      }
+    },
+    size: {
+      value: 3,
+      random: true
+    },
+    color: {
+      value: "#fff"
+    },
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: "#fff",
+      opacity: 0.3,
+      width: 1
+    },
+    move: {
+      enable: true,
+      speed: {
+        min: 0.5,
+        max: 1
+      },
+      direction: "none",
+      random: true,
+      straight: false,
+      out_mode: "bounce"
+    }
+  },
+  "retina_detect": true
+};
