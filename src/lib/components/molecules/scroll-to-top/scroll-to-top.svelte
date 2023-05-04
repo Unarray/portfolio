a06d3a3323e8012cf4f4d7c0eaa7614411f81ff7
Verre2OuiSki <48868686+Unarray@users.noreply.github.com>
2023-05-04 11:40:57 +0200

    Add component `scroll-to-top`

---
+++ src/lib/components/molecules/scroll-to-top/scroll-to-top.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  import { fly } from "svelte/transition";
+  import { ChevronUp } from "$lib/components/icons";
+  import clsx from "clsx";
+
+  let clazz = "";
+  export { clazz as class };
+  export let showOnPx = 20;
+  let y: number;
+
+  $: isVisible = y > showOnPx;
+
+  const style = clsx(
+    "border-white backdrop-blur-sm hover:bg-white/10 border-2 p-2 rounded-full",
+    "inline-block fixed bottom-8 right-8 z-40",
+    clazz
+  );
+</script>
+
+<svelte:window bind:scrollY={y} />
+
+{#if isVisible}
+  <button
+    on:click={() => document.body.scrollIntoView()}
+    class="{style}"
+    transition:fly={{ y: "4rem", duration: 700 }}
+  >
+    <ChevronUp size="1.5rem"/>
+  </button>
+{/if}
