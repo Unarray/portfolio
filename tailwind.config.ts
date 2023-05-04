d4131239c26b9392f6384fc6919e5c61519a76fc
Verre2OuiSki <48868686+Unarray@users.noreply.github.com>
2023-05-04 11:41:48 +0200

    add new text size `xxl`

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -21,14 +21,15 @@ export default {
         1: "#111"
       }
     },
-
+    
     fontSize: {
       "sm": "0.8rem",
       "base": "1rem",
       "lg": "1.125rem",
       "xl": "1.25rem",
       "2xl": "1.5rem",
-      "3xl": ["2rem", { lineHeight: "1" }]
+      "3xl": ["2rem", { lineHeight: "1" }],
+      "xxl": ["4.5rem", { lineHeight: "1" }]
     },
 
     fontFamily: {
