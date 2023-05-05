a2e457e64ef79855ba13ba2f0d04183ee953602e
Verre2OuiSki <48868686+Unarray@users.noreply.github.com>
2023-05-05 22:46:33 +0200

    Add download icon

--- src/lib/components/icons/index.ts
+++ src/lib/components/icons/index.ts
@@ -3,3 +3,4 @@ export { default as ChevronUp } from "./chevron-up.svelte";
 export { default as CircleX } from "./circle-x.svelte";
 export { default as Cross } from "./cross.svelte";
 export { default as AleterCircle } from "./alerte-circle.svelte";
+export { default as Download } from "./download.svelte";
