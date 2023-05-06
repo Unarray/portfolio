93357ab14784c8b8ad9158d4c8fa4cfebdf506ad
Verre2OuiSki <48868686+Unarray@users.noreply.github.com>
2023-05-06 11:15:22 +0200

    Start creating burger menu

---
+++ src/lib/components/molecules/burger-menu/burger-button.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  export let open = false;
+  let clazz = "";
+  export { clazz as class };
+</script>
+
+<button
+  class={clazz}
+  aria-label="Hamburger menu"
+  class:is-active={open}
+  on:click
+  on:click={() => (open = !open)}>
+  <span class="hamburger-box">
+    {open ? "CLOSE MENU" : "OPEN MENU"}
+  </span>
+</button>
---
+++ src/lib/components/molecules/burger-menu/index.ts
@@ -0,0 +1,1 @@
+export { default as BurgerButton } from "./burger-button.svelte";
