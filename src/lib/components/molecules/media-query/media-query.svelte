74a451a1a5513ce97fbc0ea33b0c7edd8b3d6f5c
Verre2OuiSki <48868686+Unarray@users.noreply.github.com>
2023-05-10 09:05:44 +0200

    Add mediaquery element

---
+++ src/lib/components/molecules/media-query/media-query.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+
+  export let query: string;
+
+  let mediaQueryList: MediaQueryList;
+  let matches = false;
+
+  onMount(() => {
+    mediaQueryList = window.matchMedia(query);
+
+    mediaQueryList.addEventListener("change", mediaQueryListEvent => matches = mediaQueryListEvent.matches);
+
+    // Init first mediaQuery
+    matches = mediaQueryList.matches;
+  });
+</script>
+
+<slot {matches} />
