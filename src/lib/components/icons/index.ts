518f4c2077bd38fe0cc2e18f33906b343ace5803
Verre2OuiSki <48868686+Unarray@users.noreply.github.com>
2023-05-12 15:21:21 +0200

    Burger menu icon

--- src/lib/components/icons/index.ts
+++ src/lib/components/icons/index.ts
@@ -4,3 +4,6 @@ export { default as CircleX } from "./circle-x.svelte";
 export { default as Cross } from "./cross.svelte";
 export { default as AleterCircle } from "./alerte-circle.svelte";
 export { default as Download } from "./download.svelte";
+export { default as Menu } from "./menu.svelte";
+
+export type { Size } from "./icons.type";
