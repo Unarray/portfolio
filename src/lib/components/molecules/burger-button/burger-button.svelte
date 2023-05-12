43b2803709b6bfb5ac9b54a15fed97db8c553a67
Verre2OuiSki <48868686+Unarray@users.noreply.github.com>
2023-05-12 14:52:16 +0200

    Create burger menu button

---
+++ src/lib/components/molecules/burger-button/burger-button.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { Cross, Menu } from "$lib/components/icons";
+  import type { Size } from "$lib/components/icons";
+
+  export let open = false;
+  export let size: Size;
+  let clazz = "";
+  export { clazz as class };
+
+  $: tag = open ? Cross : Menu;
+</script>
+
+<button
+  class={clazz}
+  aria-label="Hamburger menu"
+  on:click={() => open = !open}>
+  <svelte:component this={tag} size={size} />
+</button>
