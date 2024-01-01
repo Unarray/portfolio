2f85b9f45f5788e689ea93c9f4b0190c6ad94b07
Unarray <48868686+Unarray@users.noreply.github.com>
2024-01-01 16:13:28 +0100

    Create floating animation for skills

--- src/routes/skills.hook.ts
+++ src/routes/skills.hook.ts
@@ -1,10 +1,11 @@
 import { createBallScene, type SkillScene } from "./skills.three";
 import { technologies } from "$lib/config/techno";
-import { WebGLRenderer } from "three";
+import { Clock, MathUtils, WebGLRenderer } from "three";
 import { isDomElementVisible } from "./skills.util";
+import { floatingAnimationConfig } from "./skils.const";
 
 export const useSkillsRenderer = (canvas: HTMLCanvasElement, canvasContainer: HTMLDivElement): void => {
-  const scenes: SkillScene[] = [];
+  const scenes: {seed: number; scene: SkillScene}[] = [];
 
   // Generate skills containers elements
   for (const technologie of technologies) {
@@ -12,7 +13,10 @@ export const useSkillsRenderer = (canvas: HTMLCanvasElement, canvasContainer: HT
     div.classList.add("w-32", "h-32");
 
     canvasContainer.appendChild(div);
-    scenes.push(createBallScene(div, technologie.iconURL));
+    scenes.push({
+      seed: Math.random() * 10000,
+      scene: createBallScene(div, technologie.iconURL)
+    });
   }
 
   // Create renderer
@@ -22,22 +26,36 @@ export const useSkillsRenderer = (canvas: HTMLCanvasElement, canvasContainer: HT
     alpha: true
   });
 
-  const render = (): void => {
+  const clock = new Clock();
+  const { speed, rotationIntensity, floatIntensity, floatingRange } = floatingAnimationConfig;
+
+  const animate: FrameRequestCallback = () => {
+    requestAnimationFrame(animate);
     renderer.setSize(canvasContainer.clientWidth, canvasContainer.clientHeight);
     renderer.setScissorTest(false);
     renderer.clear();
     renderer.setScissorTest(true);
 
-    for (const scene of scenes) {
+    for (const { seed, scene } of scenes) {
       scene.control.update();
 
+      // Floating animation
+      const t = seed + clock.getElapsedTime();
+
+      scene.rotation.x = (Math.cos((t / 4) * speed) / 8) * rotationIntensity;
+      scene.rotation.y = (Math.cos((t / 4) * speed) / 8) * rotationIntensity;
+      scene.rotation.z = (Math.cos((t / 4) * speed) / 8) * rotationIntensity;
+
+      let yPosition = Math.sin((t / 4) * speed) / 10;
+      yPosition = MathUtils.mapLinear(yPosition, -0.1, 0.1, floatingRange[0], floatingRange[1]);
+      scene.position.y = yPosition * floatIntensity;
+
+      // Render scene in his contaning element
       const element = scene.element;
       const canvasRect = renderer.domElement.getBoundingClientRect();
       const sceneRect = element.getBoundingClientRect();
 
-      if (!isDomElementVisible(canvasRect)) {
-        return;
-      }
+      if (!isDomElementVisible(canvasRect)) return;
 
       const width = sceneRect.width;
       const height = sceneRect.height;
@@ -48,11 +66,6 @@ export const useSkillsRenderer = (canvas: HTMLCanvasElement, canvasContainer: HT
       renderer.setScissor(left, bottom, width, height);
       renderer.render(scene, scene.camera);
     }
-  };
-
-  const animate: FrameRequestCallback = () => {
-    render();
-    requestAnimationFrame(animate);
   };
 
   animate(0);
