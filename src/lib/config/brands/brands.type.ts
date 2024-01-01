c4e8c1bfa929b62c07c5f0d8d40b9cc7de351fff
Unarray <48868686+Unarray@users.noreply.github.com>
2024-01-01 15:43:36 +0100

    type ComponentConstructor values

--- src/lib/config/brands/brands.type.ts
+++ src/lib/config/brands/brands.type.ts
@@ -1,8 +1,12 @@
+import type { Icon, IconProps } from "@tabler/icons-svelte";
+import type { ComponentConstructorOptions } from "svelte";
+
 export type BrandsComponentsConfig = Record<
   string,
   {
-    // TODO: Find how type this things
-    base: unknown;
-    filled: unknown;
+    base: ComponentConstructor<Icon>;
+    filled: ComponentConstructor<Icon>;
   }
 >;
+
+type ComponentConstructor<T> = new (options: ComponentConstructorOptions<IconProps>) => T;
