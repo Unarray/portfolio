202f288666fdb868d6b886019453601ca5c56736
Unarray <48868686+Unarray@users.noreply.github.com>
2024-01-06 16:47:37 +0100

    Create link atom component

---
+++ src/lib/components/atoms/link/link.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+    import { cva, type VariantProps } from "class-variance-authority";
+    import { twMerge } from "tailwind-merge";
+
+  export let variant: LinkProps["variant"] = undefined;
+  export let href: string;
+  export let blankTarget = false;
+  let clazz: string | null = null;
+  export { clazz as class };
+
+  type LinkProps = VariantProps<typeof link>
+  const link = cva(
+    "",
+    {
+      variants: {
+        variant: {
+          stylizedText: [
+            "text-blue-1",
+            "hover:underline"
+          ],
+          notStylizedText: []
+        }
+      },
+      defaultVariants: {
+        variant: "stylizedText"
+      }
+    }
+  );
+
+</script>
+
+<a class={twMerge(link({ variant }), clazz)} target={blankTarget ? "_blank" : "_self"} href={href}>
+  <slot/>
+</a>
